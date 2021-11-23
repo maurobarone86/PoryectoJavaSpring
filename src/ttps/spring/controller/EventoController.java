d55d2d75c84906e4bb978f48aa0efdaa9b351b6a
Denis Cruz <denis99lp@gmail.com>
2021-11-23 12:15:57 -0300

    pom

--- src/ttps/spring/controller/EventoController.java
+++ src/ttps/spring/controller/EventoController.java
@@ -1,10 +1,70 @@
 package ttps.spring.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ttps.spring.model.model.Evento;
+import ttps.spring.service.EventoService;
+
 @RestController
-@RequestMapping("/evento")
+@RequestMapping("/apiEvento")
 public class EventoController {
 
+	@Autowired
+	private EventoService eventoService;
+
+	@GetMapping("/eventos/lista")
+	public ResponseEntity<List<Evento>> obtenerLista(){
+		List<Evento> lista= eventoService.recuperarTodos();
+		if (lista!=null) {
+			return new ResponseEntity<List<Evento>>(lista, HttpStatus.OK);
+		}
+		return new ResponseEntity<List<Evento>>(HttpStatus.NO_CONTENT);
+	}
+	@GetMapping("/{id}")
+	public ResponseEntity<Evento> obtener(@PathVariable("id") Long id){
+		Evento evento= eventoService.findById(id);
+		if (evento!=null) {
+			return new ResponseEntity<Evento>(evento, HttpStatus.OK);
+		}
+		return new ResponseEntity<Evento>(HttpStatus.NO_CONTENT);
+	}
+	
+	@PostMapping("/guardar")
+	public ResponseEntity<Evento> guardar(@RequestBody Evento evento){
+		Evento eventoG= eventoService.guardar(evento);
+		if (eventoG!=null) {
+			return new ResponseEntity<Evento>(eventoG, HttpStatus.OK);
+		}
+		return new ResponseEntity<Evento>(HttpStatus.NO_CONTENT);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Boolean> deleteEvento(@PathVariable("id") Long id){
+		Boolean dato= eventoService.deleteEvento(id);
+		if (dato) {
+			return new ResponseEntity<Boolean>(dato, HttpStatus.OK);
+		}
+		return new ResponseEntity<Boolean>(HttpStatus.NOT_FOUND);
+	}
+	
+	@PutMapping("/modificar/{id}")
+	public ResponseEntity<Evento> modificarEvento(@PathVariable("id") Long id, @RequestBody Evento nuevo){
+		Evento evento= eventoService.modificar(id, nuevo);
+		if (evento!=null) {
+			return new ResponseEntity<Evento>(evento, HttpStatus.OK);
+		}
+		return new ResponseEntity<Evento>(HttpStatus.NO_CONTENT);
+	}
 }
--- src/ttps/spring/service/EventoService.java
+++ src/ttps/spring/service/EventoService.java
@@ -1,9 +1,14 @@
 package ttps.spring.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import ttps.spring.model.dao.EventoDAO;
+import ttps.spring.model.model.Evento;
 import ttps.spring.model.repository.EventoRepository;
 
 @Service
@@ -12,17 +17,73 @@ public class EventoService {
 	@Autowired
 	private EventoRepository eventoRepository;
 
-	public EventoService(EventoRepository eventoRepository) {
+	@Autowired
+	private EventoDAO eventoDAO;
+	
+	public EventoService(EventoRepository eventoRepository, EventoDAO eventoDAO) {
 		super();
 		this.eventoRepository = eventoRepository;
+		this.eventoDAO=eventoDAO;
 	}
 
 	public EventoRepository getEventoRepository() {
 		return eventoRepository;
+	}
+	
+	public EventoDAO getEventoDAO() {
+		return eventoDAO;
+	}
+	public void setEventoDAO(EventoDAO eventoDAO) {
+		this.eventoDAO=eventoDAO;
 	}
 
 	public void setEventoRepository(EventoRepository eventoRepository) {
 		this.eventoRepository = eventoRepository;
 	}
 
+	public Evento findById(Long id) {
+		Optional<Evento> evento= getEventoRepository().findById(id);
+		if (evento.isEmpty()) {
+			return null;
+		}
+		return evento.get();
+	}
+	
+	public List<Evento> recuperarTodos(){
+		return getEventoRepository().findAll();
+	}
+	
+	public Evento guardar(Evento evento) {
+		if (evento.getNombre()!=null) {
+			return getEventoRepository().save(evento);
+		}
+		return null;
+	}
+	
+	public Boolean deleteEvento(Long id) {
+		Evento evento=findById(id);
+		if (evento!=null) {
+			getEventoRepository().delete(evento);
+			return true;
+		}
+		return false;
+	}
+	
+	public Evento modificar(Long id, Evento nuevo) {
+		Evento evento=findById(id);
+		if (evento!=null) {
+			evento.setNombre(nuevo.getNombre());
+			evento.setActivo(nuevo.getActivo());
+			evento.setCodigoPostal(nuevo.getCodigoPostal());
+			evento.setProvincia(nuevo.getProvincia());
+			evento.setEstado(nuevo.getEstado());
+			evento.setFecha(nuevo.getFecha());
+			evento.setGeolocalizacion(nuevo.getGeolocalizacion());
+			evento.setHora(nuevo.getHora());
+			evento.setReservas(nuevo.getReservas());
+			evento.setTipo(nuevo.getTipo());
+			return getEventoRepository().save(evento);
+		}
+		return null;
+	}
 }

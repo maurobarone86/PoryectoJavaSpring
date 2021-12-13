8a7f70d50afc2c71b1f0edfb83fa1cade0ed6973
Barone, Mauro Cesar <mauro_barone86@hotmail.com>
2021-12-12 23:23:19 -0300

    modifico back para utilizar la clase Usuario

--- src/ttps/spring/controller/LoginController.java
+++ src/ttps/spring/controller/LoginController.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 //import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import ttps.spring.model.dao.Usuario1DAO;
+//import ttps.spring.model.dao.Usuario1DAO;
+import ttps.spring.model.model.Usuario;
 import ttps.spring.model.model.Credentials;
-import ttps.spring.model.model.UsernaneAndPassword;
-import ttps.spring.model.model.Usuario1;
+//import ttps.spring.model.model.UsernaneAndPassword;
+//import ttps.spring.model.model.Usuario1;
 import ttps.spring.service.TokenServices;
+import ttps.spring.service.UsuarioService;
 
 /**
  * @author manuel
@@ -19,15 +21,19 @@ import ttps.spring.service.TokenServices;
 @RestController
 public class LoginController {
 
-    @Autowired
-    private Usuario1DAO usuario1DAO;
+    //@Autowired
+    //private Usuario1DAO usuario1DAO;
 
     @Autowired
     private TokenServices tokenServices;
 
-    // un dia
-    private final int EXPIRATION_IN_SEC = 100;
+    // 20 minutos dura el token
+    private final int EXPIRATION_IN_SEC = 1200;
+    
+    @Autowired
+	private UsuarioService usuarioService;
 
+    /*
     @PostMapping(path = "/auth")
     public ResponseEntity<?> authenticate(@RequestBody UsernaneAndPassword userpass) {
 
@@ -39,12 +45,33 @@ public class LoginController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Usuario o password incorrecto");
         }
     }
-
+    
     private boolean isLoginSuccess(String username, String password) {
         // recupero el usuario de la base de usuarios
         Usuario1 u = usuario1DAO.getUsuarioPorUsername(username);
 
         // chequeo que el usuario exista y el password sea correcto
         return (u != null && u.getPassword().equals(password));
+    }   */
+    
+    @PostMapping(path = "/auth")
+    public ResponseEntity<?> authenticate(@RequestBody Usuario usuario) {
+
+        if(loguinCorrecto(usuario)) {
+        	//System.out.println("Ingreso al login");
+            String token = tokenServices.generateToken(usuario.getNombreUsuario(), EXPIRATION_IN_SEC);
+            return ResponseEntity.ok(new Credentials(token, EXPIRATION_IN_SEC, usuario.getNombreUsuario()));
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Usuario o password incorrecto");
+        }
     }
+        
+    private boolean loguinCorrecto(Usuario usuarioIngresado) {
+    	boolean resultado = false;
+    	Usuario usuario = usuarioService.loginUser(usuarioIngresado);
+    	if (usuario != null) {resultado=true;}
+    	return resultado;
+    }
+
+   
 }
--- src/ttps/spring/service/TokenServices.java
+++ src/ttps/spring/service/TokenServices.java
@@ -8,23 +8,11 @@ import java.security.Key;
 import java.util.Calendar;
 import java.util.Date;
 
-/**
- * Servicio para gestionar los tokens de authenticacion
- *
- * @author manuel
- */
 @Service
 public class TokenServices {
 
     final static Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
-    /**
-     * Genera el token de authorizacion para el usuario
-     *
-     * @param username Username que se guarda dentro del token
-     * @param segundos tiempo de validez del token
-     * @return token
-     */
     public String generateToken(String username, int segundos) {
 
         Date exp = getExpiration(new Date(), segundos);
@@ -32,16 +20,9 @@ public class TokenServices {
         return Jwts.builder().setSubject(username).signWith(key).setExpiration(exp).compact();
     }
 
-    /**
-     * Retorna la suma de <code>segundos</code> a la <code>fecha</code>
-     *
-     * @param date
-     * @param segundos
-     * @return
-     */
     private Date getExpiration(Date date, int segundos) {
         Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date); // Configuramos la fecha que se recibe
+        calendar.setTime(date); 
         calendar.add(Calendar.SECOND, segundos);
 
         return calendar.getTime();
@@ -55,24 +36,17 @@ public class TokenServices {
             if (token.startsWith(prefix)) {
                 token = token.substring(prefix.length()).trim();
             }
-            /*
-            Claims claims = Jwts.parser()
-                    .setSigningKey(key)
-                    .parseClaimsJws(token).getBody();*/
+           
             
             Claims claims = Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
 
-            System.out.println("ID: " + claims.getId());
-            System.out.println("Subject: " + claims.getSubject());
-            System.out.println("Issuer: " + claims.getIssuer());
-            System.out.println("Expiration: " + claims.getExpiration());
-
+           
             return true;
         } catch (ExpiredJwtException exp) {
             System.out.println("El Token es valido, pero expiro su tiempo de validez");
             return false;
         } catch (JwtException e) {
-            // Algo salio mal en la verificacion
+            
             System.out.println("Error: " + e.getMessage());
             return false;
         }

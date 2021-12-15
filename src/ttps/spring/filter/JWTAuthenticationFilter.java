f9e56d0d9af0c16aecd98beea3b387e82d1905d6
Denis Cruz <denis99lp@gmail.com>
2021-12-14 22:19:08 -0300

    habilito los servicios en el filter

--- src/ttps/spring/filter/JWTAuthenticationFilter.java
+++ src/ttps/spring/filter/JWTAuthenticationFilter.java
@@ -30,8 +30,8 @@ public class JWTAuthenticationFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
 
-        // El login del usuarios es publico
-        if ("/proyectoJavaSpring/auth".equals(req.getRequestURI()) ||
+        // El login del usuarios es publico y los servicios
+        if ("/proyectoJavaSpring/auth".equals(req.getRequestURI()) || "/proyectoJavaSpring/apiServicio/serviciosActivos".equals(req.getRequestURI()) ||
                 HttpMethod.OPTIONS.matches(req.getMethod())) {
 
             chain.doFilter(request, response);
